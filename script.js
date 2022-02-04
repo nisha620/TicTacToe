a3e9002cf00af3feed214b40536190f042a1eeeb
Kumarsam17 <50691250+Kumarsam17@users.noreply.github.com>
2022-02-04 14:29:56 +0530

    1st commit need to improve more

---
+++ script.js
@@ -0,0 +1,18 @@
+let turn ='X';
+
+function changeTurn() {
+    return turn==="X"?"O":"X"
+}
+
+//game logic
+
+let boxes =document.getElementsByClassName("boxes");
+Array.from(boxes).forEach(element=>{
+    let boxtext =element.querySelector('.boxtext');
+    element.addEventListener('click',function(){
+        if(boxtext.innerText==''){
+            boxtext.innerText=turn;
+            turn=changeTurn()
+        }
+    })
+})
